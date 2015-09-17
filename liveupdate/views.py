7f324b94ca4bf90bf9d7abdee41a8cdffcd5e322
olgaklimenko <olga@debian.local>
2015-09-18 04:32:41 +0800

    Update

--- liveupdate/views.py
+++ liveupdate/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
+from django.core import serializers
+from django.http import HttpResponse
 from liveupdate.models import Update
 
 class ListView(generic.ListView):
@@ -8,3 +10,9 @@ class ListView(generic.ListView):
     def get_queryset(self):
         return Update.objects.all()
 
+def updates_after(request, id):
+    response = HttpResponse()
+    response['Content-Type'] = "text/javascript"
+    response.write(serializers.serialize("json",
+        Update.objects.filter(pk__gt=id)))
+    return response
